5c9f3458defde3b7f86070cda78d3b1e06011a68
Marlon Santos <marlon@marlsantos.com>
2016-09-04 23:19:52 +1000

    Alpha implementation complete.

--- spam-assassin/index.php
+++ spam-assassin/index.php
@@ -6,8 +6,8 @@ class SpamAssassinPlugin extends \RainLoop\Plugins\AbstractPlugin
 	
 	public function Init()
 	{
-		$this->addHook('filter.login-credentials', 'FilterLoginCredentials');
-		$this->addHook('filter.post-config', 'FilterPostAction');
+		$this->addHook('filter.post-update', 'FilterSpamAssassinConfig');
+		$this->addHook('filter.spam-assassin', 'FilterSpamAssassin');
 		$this->addJs('js/app.js', true);
 		
 		$config_file = \trim($this->Config()->Get('plugin', 'spam_assassin_config', ''));
@@ -25,32 +25,15 @@ class SpamAssassinPlugin extends \RainLoop\Plugins\AbstractPlugin
 	}
 
 	/**
-	 * @param string $sEmail
-	 * @param string $sLogin
-	 * @param string $sPassword
-	 *
-	 * @throws \RainLoop\Exceptions\ClientException
-	 */
-	public function FilterLoginCredentials(&$sEmail, &$sLogin, &$sPassword)
-	{
-		$sBlackList = \trim($this->Config()->Get('plugin', 'spam_assassin_blacklist', ''));
-		if (0 < \strlen($sBlackList) && \RainLoop\Plugins\Helper::ValidateWildcardValues($sEmail, $sBlackList))
-		{
-			$sWhiteList = \trim($this->Config()->Get('plugin', 'spam_assassin_whitelist', ''));
-			if (0 === \strlen($sExceptions) || !\RainLoop\Plugins\Helper::ValidateWildcardValues($sEmail, $sWhiteList))
-			{
-				throw new \RainLoop\Exceptions\ClientException(
-					 \RainLoop\Notifications::AccountNotAllowed);
-			}
-		}
-	}
-
-	/**
 	 * @return array
 	 */
 	public function configMapping()
 	{	
 		return array(
+			\RainLoop\Plugins\Property::NewInstance('spam_assassin_wildcards')->SetLabel('Treat as wildcard domains')
+				->SetType(\RainLoop\Enumerations\PluginPropertyType::BOOL)
+				->SetDescription('Determines whether emails are treated as wildcard *@domain.com')
+				->SetDefaultValue(true),
 			\RainLoop\Plugins\Property::NewInstance('spam_assassin_config')
 			    ->SetLabel('SpamAssassin Config filepath')
 				->SetAllowedInJs(true)
@@ -111,21 +94,89 @@ class SpamAssassinPlugin extends \RainLoop\Plugins\AbstractPlugin
 		$this->Config()->Save();
 	}
 	
-	public function FilterPostAction(&$aMap, &$oConfig) {
-		error_log("*** DEBUG: FilterPostAction(".$this->config_file.")");
+	public function FilterSpamAssassinConfig(&$aMap, &$oConfig) {
 		$bWrite = false;
 		foreach ($aMap as $oItem)
 		{
 			$sValue = $oConfig->Get('plugin', $oItem->Name());
-			//error_log($oItem->Name()." => ". $oConfig->Get('plugin', $oItem->Name()));
 			if ($this->config_file == $sValue) {
 				if ($this->checkPermissions()) {
 					$bWrite = true;
 				}
 			} elseif ($bWrite) {
-				error_log(" *** Writing ".$sValue);
+				 $this->FilterSpamAssassin(str_replace(' ', ',', $sValue), $oItem->Name());
 			}
 		}
+		
+	}
+	/**
+	 * Action DoMessageMove() hook to write emails captured into Spam Assassin's config file
+	 */
+	public function FilterSpamAssassin($sEmailFrom, $sList, $bAdmin = true) {
+		
+		$aEmails = explode(',', $sEmailFrom);
+		$this->Config()->Load();
+		$sConfig = $this->Config()->Get('plugin', 'spam_assassin_config', '');
+		$bWildCard = $this->Config()->Get('plugin', 'spam_assassin_wildcards', '');
+
+		$arrEmails = array();
+		foreach ($aEmails as $sEmail) {
+			$re = "/(.*[^*])@(.*)/m";
+			$subst = "*@$2"; 
+			if ($bWildCard && preg_match($re, $sEmail)) {
+				$sEmail = preg_replace($re, $subst, $sEmail);
+			}
+			array_push($arrEmails, $sEmail);
+		}
+	
+		
+	
+		$handle = fopen($sConfig, 'r');
+		$out = array();
+		if ($handle) {
+			$re = $config_list = '';
+			 if (is_int(stripos($sList, 'blacklist'))) {
+			 	$re = "/(blacklist_from\s)(.*)/m";
+			 	$config_list = "blacklist_from ";
+			 } elseif (is_int(stripos($sList, 'whitelist'))) {
+			 	$re = "/(whitelist_from\s)(.*)/m";
+			 	$config_list = "whitelist_from ";
+			 }
+			
+		 	 while (($line = fgets($handle)) !== false) {
+		 		if (preg_match($re, $line, $ematch)) {
+		 			//For webmail public, we append existing 
+		 			//emails from config file
+		 			if (!$bAdmin) {
+		 				array_push($arrEmails, $ematch[2]);
+		 			}
+		 		} else {
+		 			if (strlen(trim($line)) > 0) {
+		 				array_push($out, $line);
+		 			}
+		 		}
+		 	 }
+		 	 //We don't want duplicates
+		 	 $arrEmails = array_unique($arrEmails);
+		 	 fclose($handle);
+		 	 
+			 //We write new config file without the list emails 
+			 $fp = fopen($sConfig, "w+");
+			 flock($fp, LOCK_EX);
+			 foreach($out as $line) {
+			     fwrite($fp, $line);
+			 }
+			 
+			 //Then we add the list emails at the bottom
+			 foreach(array_filter($arrEmails) as $email) {
+			 	$line = $config_list.$email."\n";
+			 	fwrite($fp, $line);
+			 }
+			 
+			 flock($fp, LOCK_UN);
+			 fclose($fp);  
+		}
+		
 		
 	}
 	
--- spam-assassin/js/app.js
+++ spam-assassin/js/app.js
@@ -1,17 +1,35 @@
 (function ($, window) {
     
 	$(function () {
-
+		console.debug('*** Loaded spam-assassin JS plugin(tm) ***');
 		if (window.rl && window.rl.settingsGet('Auth'))
 		{
-			//Capture spam button click email senders
-			$( document ).on( "click", "a.button-spam", function() { 
-			    var sel = $('i.checkboxMessage.icon-checkbox-checked');
-			    $.each(sel, function( index, div ) {
-			        var email = $(div).parent().parent().find('.senderParent').find('span.sender').attr('title');
-			        console.debug(email);
-			    });
-			});
+			var sSpamConfig = window.rl.pluginSettingsGet('spam-assassin', 'spam_assassin_config');
+			if (sSpamConfig) {
+				window._spamassassin = window._spamassassin || [];
+				//Capture spam button click email senders
+				$( document ).on( "click", "a.button-spam", function() { 
+				    var sel = $('i.checkboxMessage.icon-checkbox-checked');
+				    $.each(sel, function( index, div ) {
+				        var email = $(div).parent().parent().find('.senderParent').find('span.sender').attr('title');
+				        window._spamassassin.push(email);
+				    });
+				});
+				window.rl.addHook('ajax-default-request', function (sAction, oParameters) {
+					if ('MessageMove' === sAction && oParameters)
+					{
+						oParameters['EmailFrom'] = window._spamassassin.toString();
+					}
+				});
+				
+				window.rl.addHook('ajax-default-response', function (sAction, oData, sType) {
+					if ('MessageMove' === sAction && sType)
+					{
+						window._spamassassin = [];
+					}
+				});
+			}
+			
 		}
 
 	});
